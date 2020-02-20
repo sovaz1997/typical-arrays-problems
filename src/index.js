d170c353935033ab999e6067b707fffb53a9b346
sovaz1997 <sovaz1997@yandex.ru>
2020-02-20 19:34:37 +0300

    refactor: refactor

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 
 exports.min = function min (array) {
-  if(array === undefined || array.length === 0) {
-    return 0;
-  }
-
-  let min = array[0];
-  array.forEach(element => {
-    min = Math.min(min, element);
-  });
-
-  return min;
+  if(array === undefined || array.length === 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-    if(array === undefined || array.length === 0) {
-        return 0;
-    }
-    
-    let max = array[0];
-    array.forEach(element => {
-        max = Math.max(max, element);
-    });
-    
-    return max;
+    if(array === undefined || array.length === 0) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-    if(array === undefined || array.length === 0) {
-        return 0;
-    }
-    
-    let avg = 0;
-    array.forEach(element => {
-        avg += element;
-    });
-    
-    return avg / array.length;
+    if(array === undefined || array.length === 0) return 0;
+    const sum = array.reduce((sum, el) => sum + el, 0);
+    return sum / array.length;
 }
